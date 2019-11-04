9f2be4a9d9960375397f60ebacad9a0b7a53778c
Paulo Rodriguez <pauloxat8@gmail.com>
2019-11-04 05:49:25 -0300

    Version 1.0
    Fixed, /aon is for added as friend...

--- plugins/nofollow.py
+++ plugins/nofollow.py
@@ -1,5 +1,5 @@
 def plugin(core, packet, direction, user):
     if direction == 'toxat' and packet['name'] == 'z':
         if packet['t'] != '/l':
-            packet['t'] = '/axat'
+            packet['t'] = '/a_NF'
         return packet
