66885a79bc31008278b935f4ca48831ffefe024b
Paulo Rodriguez <pauloxat8@gmail.com>
2019-11-22 21:30:28 -0300

    Not used anymore.

--- plugins/connected.py
+++
@@ -1,3 +0,0 @@
-def plugin(core, packet, direction, user):
-    if direction == 'fromxat' and packet['name'] == 'done':
-        user.send(core.buildPacket('m', {'t': 'xatClient is working...', 'u': 0}))
